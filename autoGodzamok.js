e7231616aa29ec44668489be66c65b911e347cff
BotBoi345 <islamic60seconds@gmail.com>
2024-12-08 21:46:08 +0530

    Update autoGodzamok.js

--- autoGodzamok.js
+++ autoGodzamok.js
@@ -43,7 +43,7 @@ javascript:( function () {
 	}
 	//sell and rebuy buildings for click frenzy
 	function activateGodzamok(){
-		var buildingList = [0,2,3,4,5,0,0];
+		var buildingList = [2,3,4,5,8,9];
 		if(Game.hasGod('ruin')){
 			for( var theBuilding in buildingList ){
 				var numCurrentBuilding = Game.ObjectsById[buildingList[theBuilding]].amount;
