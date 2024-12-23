17676cf86a03139819ee01cb4af413c29a5c1961
BotBoi345 <islamic60seconds@gmail.com>
2024-12-23 23:04:31 +0530

    Update test.js

--- test.js
+++ test.js
@@ -43,13 +43,12 @@ javascript:( function () {
 	}
 	//sell and rebuy buildings for click frenzy
 	function activateGodzamok(){
-		var buildingList = [2,3,4,5]; 
+		var buildingList = [2,3,4,5];
 		l('storeBulkBuy').click();
 		l('storeBulk1').click();
 		for( var theBuilding in buildingList ){
 			var numCurrentBuilding = Game.ObjectsById[buildingList[theBuilding]].amount;
 			Game.ObjectsById[buildingList[theBuilding]].sell(numCurrentBuilding);
-			// rebuy all buildings
 			Game.ObjectsById[buildingList[theBuilding]].buy(numCurrentBuilding); 
 		}
 	}
