cbb1694751f86a9fdbb9685b8d9cbd6c48084230
BotBoi345 <islamic60seconds@gmail.com>
2025-01-09 23:33:48 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,13 +1,8 @@
 function activate(p, q) {
         Game.Notify(p + '' + q + ' loaded!', '', '', 2, 2);
-        try {
-                while (Game.ObjectsbyId[p].amount < q) {
-                        if (Game.cookies >= Game.ObjectsbyId[p]) {
-                                Game.ObjectsbyId[p].buy();
-                        }
+        while (Game.ObjectsbyId[p].amount < q) {
+                if (Game.cookies >= Game.ObjectsbyId[p]) {
+                        Game.ObjectsbyId[p].buy();
                 }
-        }
-        catch(err) {
-                Game.Notify('Error', err.message, '', 2, 2);
         }
 }
