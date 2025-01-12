b49ea3e33e9a1fce4b50c6730dfbde86c1442816
BotBoi345 <islamic60seconds@gmail.com>
2025-01-12 19:01:59 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,5 +1,5 @@
 function activate(p, q) {
-        Game.Notify(p + '' + q + ' loaded!', '', '', 2, 2);
+        Game.Notify(`Buying ${Game.ObjectsById[p].dname}s till ${q}`, '', '', 2, 2);
         while (Game.ObjectsById[p].amount < q) {
                 if (Game.cookies >= Game.ObjectsById[p].bulkPrice) {
                         Game.ObjectsById[p].buy();
