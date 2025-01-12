9e26a17602e19743388b8061d411fc227959ab29
BotBoi345 <islamic60seconds@gmail.com>
2025-01-12 21:36:01 +0530

    Update try.js

--- try.js
+++ try.js
@@ -2,9 +2,12 @@ Game.Notify(`AutoBuy Loaded!`, `AutoBuy has been loaded successfully.`, [10,6], 
 function activate(p, q) {
         try {
                 Game.Notify(`AutoBuy Started`, `Buying ${Game.ObjectsById[p].dname}s till ${q}`, [2,6], 2, 2);
-                while (Game.ObjectsById[p].amount < q) {
+                function buy() {
                         if (Game.cookies < Game.ObjectsById[p].bulkPrice) continue;
                         Game.ObjectsById[p].buy();
+                }
+                while (Game.ObjectsById[p].amount < q) {
+                        const buy = setTimeout(buy, 1000);
                 }
                 Game.Notify(`Done!`,'','',2,2);
         }
