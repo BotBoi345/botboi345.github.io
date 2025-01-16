7d88a706a87e1561de3909bd2f807045326fedf4
BotBoi345 <islamic60seconds@gmail.com>
2025-01-16 19:26:14 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,16 +1,10 @@
 Game.Notify(`AutoBuy Loaded!`, `AutoBuy has been loaded successfully.`, [10,6], 2, 2);
 function activate(p, q) {
-        try {
-                Game.Notify(`AutoBuy Started`,`Buying ${Game.ObjectsById[p].dname}s till ${q}`,[2,6],2,2); 
-                
-                for (; Game.ObjectsById[p].amount < q && Game.cookies >= Game.ObjectsById[p].bulkPrice; ) {
-                        Game.ObjectsById[p].buy;
-                }
-                
-                Game.Notify(`Done!`,'','',2,2);
+        Game.Notify(`AutoBuy Started`,`Buying ${Game.ObjectsById[p].dname}s till ${q}`,[2,6],2,2); 
+        
+        for (; Game.ObjectsById[p].amount < q && Game.cookies >= Game.ObjectsById[p].bulkPrice; ) {
+                Game.ObjectsById[p].buy;
         }
         
-        catch(err) {
-                Game.Notify(`Error Occured`, `${err.message}. Try Again...`, [16,5], 2, 2);
-        } 
+        Game.Notify(`Done!`,'','',2,2);
 }
