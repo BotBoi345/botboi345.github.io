cf232e888b49a716bc2e862c8411657b296305da
BotBoi345 <islamic60seconds@gmail.com>
2025-01-19 12:54:28 +0530

    Update try.js

--- try.js
+++ try.js
@@ -1,6 +1,6 @@
 Game.Notify(`AutoBuy Loaded!`, `AutoBuy has been loaded successfully.`, [10,6], 2, 2);
-function activate(p, q) {
-        try {
+try {
+        function activate(p, q) {
                 Game.Notify(`AutoBuy Started`,`Buying ${Game.ObjectsById[p].dname}s till ${q}`,[2,6],2,2); 
 
                 const buy = setInterval(autobuy, 500);
@@ -17,7 +17,7 @@ function activate(p, q) {
                         }
                 }
         }
-        catch(err) {
-                Game.Notify(`Error Occured`, `${err.message}. Try Again...`, [16,5], 2, 2);
-        }
+}
+catch(err) {
+        Game.Notify(`Error Occured`, `${err.message}. Try Again...`, [16,5], 2, 2);
 }
