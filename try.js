04d130306651f05072b746c1bb5a147e6e056fb8
BotBoi345 <islamic60seconds@gmail.com>
2025-01-22 21:32:15 +0530

    Update try.js

--- try.js
+++ try.js
@@ -31,7 +31,13 @@ try {
                                 Game.buyBulk = 100;
                                 if (Game.cookies >= Game.ObjectsById[value].bulkPrice) {
                                         Game.ObjectsById[value].buy();
-                                        Game.Notify(`Bought ${Game.ObjectsById[value].dname}`s,`Bought 100 ${Game.ObjectsById[value].dname}s`,[2,6],2,2);
+                                        Game.Notify(
+                                                `Bought ${Game.ObjectsById[value].dname}`s,
+                                                `Bought 100 ${Game.ObjectsById[value].dname}s`,
+                                                [2,6],
+                                                2,
+                                                2
+                                        );
                                 }
                                 else {continue; }
                         }
@@ -40,7 +46,13 @@ try {
                                 Game.buyBulk = 10;
                                 if (Game.cookies >= Game.ObjectsById[value].bulkPrice) {
                                         Game.ObjectsById[value].buy();
-                                        Game.Notify(`Bought ${Game.ObjectsById[value].dname}s`,`Bought 10 ${Game.ObjectsById[value].dname}s`,[2,6],2,2);
+                                        Game.Notify(
+                                                `Bought ${Game.ObjectsById[value].dname}s`,
+                                                `Bought 10 ${Game.ObjectsById[value].dname}s`,
+                                                [2,6],
+                                                2,
+                                                2
+                                        );
                                 }
                                 else {continue; }
                         }
