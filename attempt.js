fab122d134a014391636d688a2fca8f232954ae6
BotBoi345 <islamic60seconds@gmail.com>
2025-01-28 23:18:40 +0530

    Update attempt.js

--- attempt.js
+++ attempt.js
@@ -18,7 +18,55 @@ function setGodzamokInterval(){
 			Game.hasBuff('Deduplication') || Game.hasBuff('Lab Disaster') 
 		) && !Game.hasBuff('Devastation')
 		){
-			activateGodzamok();
+			if(Game.hasGod('ruin')){
+				var numCurrentBuilding = Game.ObjectsById[0].amount;
+		
+				l('storeBulkSell').click();
+				l('storeBulkMax').click();
+				Game.ObjectsById[0].buy();
+
+				// rebuy all buildings
+				l('storeBulkBuy').click();
+				l('storeBulk1').click();
+				Game.ObjectsById[0].buy(numCurrentBuilding);
+			}
 		}
 	}
 }
+
+var godzamokInterval1 = setInterval(setGodzamokInterval1, 200);
+
+function setGodzamokInterval1(){
+	if(Game.hasGod('ruin')){
+		if( (
+			Game.hasBuff('Elder frenzy') || Game.hasBuff('Dragon Harvest') || 
+			Game.hasBuff('Click frenzy') || Game.hasBuff('Cursed finger') || 
+			Game.hasBuff('Dragonflight') || Game.hasBuff('Fervent adoration') || 
+			Game.hasBuff('Congregation') || Game.hasBuff('Righteous cataclysm') || 
+			Game.hasBuff('Ore vein') || Game.hasBuff('Luxuriant harvest') || 
+			Game.hasBuff('Manabloom') || Game.hasBuff('Delicious lifeforms') || 
+			Game.hasBuff('Juicy profits') || Game.hasBuff('High-five') || 
+			Game.hasBuff('Breakthrough') || Game.hasBuff('Oiled-up') || 
+			Game.hasBuff('Golden ages') || Game.hasBuff('Extra cycles') || 
+			Game.hasBuff('Solar flare') || Game.hasBuff('Winning streak') || 
+			Game.hasBuff('Macrocosm') || Game.hasBuff('Refactoring') || 
+			Game.hasBuff('Cosmic nursery') || Game.hasBuff('Brainstorm') ||
+			Game.hasBuff('Deduplication') || Game.hasBuff('Lab Disaster') 
+		) && !Game.hasBuff('Devastation')
+		){
+			if(Game.hasGod('ruin')){
+				var numCurrentBuilding = Game.ObjectsById[2].amount;
+		
+				l('storeBulkSell').click();
+				l('storeBulkMax').click();
+				Game.ObjectsById[2].buy();
+
+				// rebuy all buildings
+				l('storeBulkBuy').click();
+				l('storeBulk1').click();
+				Game.ObjectsById[2].buy(numCurrentBuilding);
+			}
+		}
+	}
+}
+
