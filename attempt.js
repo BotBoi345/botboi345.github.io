b3c5061f8639c0b5dc8b0bfd887bdbbc267b3a84
BotBoi345 <islamic60seconds@gmail.com>
2025-01-28 23:16:41 +0530

    Create attempt.js

---
+++ attempt.js
@@ -0,0 +1,24 @@
+var godzamokInterval = setInterval(setGodzamokInterval, 200);
+
+function setGodzamokInterval(){
+	if(Game.hasGod('ruin')){
+		if( (
+			Game.hasBuff('Elder frenzy') || Game.hasBuff('Dragon Harvest') || 
+			Game.hasBuff('Click frenzy') || Game.hasBuff('Cursed finger') || 
+			Game.hasBuff('Dragonflight') || Game.hasBuff('Fervent adoration') || 
+			Game.hasBuff('Congregation') || Game.hasBuff('Righteous cataclysm') || 
+			Game.hasBuff('Ore vein') || Game.hasBuff('Luxuriant harvest') || 
+			Game.hasBuff('Manabloom') || Game.hasBuff('Delicious lifeforms') || 
+			Game.hasBuff('Juicy profits') || Game.hasBuff('High-five') || 
+			Game.hasBuff('Breakthrough') || Game.hasBuff('Oiled-up') || 
+			Game.hasBuff('Golden ages') || Game.hasBuff('Extra cycles') || 
+			Game.hasBuff('Solar flare') || Game.hasBuff('Winning streak') || 
+			Game.hasBuff('Macrocosm') || Game.hasBuff('Refactoring') || 
+			Game.hasBuff('Cosmic nursery') || Game.hasBuff('Brainstorm') ||
+			Game.hasBuff('Deduplication') || Game.hasBuff('Lab Disaster') 
+		) && !Game.hasBuff('Devastation')
+		){
+			activateGodzamok();
+		}
+	}
+}
